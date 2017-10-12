62ef927703783900cbe23025700a0d4ceccec308
dases <dases@protonmail.com>
2017-10-12 06:29:57 -0700

    vectorHelper changed

--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -1,4 +1,43 @@
 
-public class VectorHelper {
+public class VectorHelper 
+{
+	private int[] table_1;
+	private int[] table_2;
+	
+	
+/*
+ * @param	 
+ * @return
+ *  ajouter un element num a une des deux vecteur	
+ **/
 
+	public void addElement(int num,int[] table){
+		   table[table.length] = num;	   
+	}
+	
+	
+/* @param
+ * @return
+ * trier les elements d'un tableau
+ */
+	public void TriTable(int[] table){}
+	
+/*
+ * @param
+ * @return
+ * Sommer deux victeurs
+ */	
+	public int[] sommeVecteur(int[] table1,int[] table2){
+		return null;
+	}
+/*
+ * @param
+ * @return
+ * Inverser un vecteur
+ * @example {1,2,3} --> {3,2,1}
+ */	
+    public int[] inverseVecteur(int[] table){
+    	return null;
+    }
+	
 }
